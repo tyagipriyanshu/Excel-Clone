d9e7ef3cc3cd2eee645623be69bf189682a50dfb
Priyanshu Tyagi <tyagiabhinav43@yahoo.in>
2021-08-22 17:00:22 +0530

    updated

--- Excel Clone/poc/domExample.js
+++ Excel Clone/poc/domExample.js
@@ -1,5 +1,6 @@
 // ===========Document Object Model (DOM)============
-// -> is a programming interface (API) for web documents. 
+// -> is a programming interface (API) for web documents.
+// -> When a web page is loaded, the browser creates a DOM of the page. 
 // -> represents the page so that programs can change the document structure, style, and content. 
 // -> represents the HTML elements as nodes and objects. 
 // -> so that programming languages can interact with the HTML page.
@@ -11,6 +12,7 @@
 // let input = document.querySelector(".input");
 // let button = document.querySelector(".add");
 let ul = document.querySelector("ul");
+
 // button.addEventListener("click", function () {
 //     let ans = input.value;
 //     // alert(ans);
@@ -19,7 +21,8 @@ let ul = document.querySelector("ul");
 //     li.setAttribute("class", "task");
 //     ul.appendChild(li);
 //     input.value = "";
-// })
+// });
+
 for (let i = 0; i < 26; i++) {
     // let li = document.createElement("li");
     // li.innerText = String.fromCharCode(65 + i);
